dfdc6fc9459f1dcb565adf0bad64bff8c673e777
Marianne Adams <meadams2@bsu.edu>
2025-01-31 14:39:41 +0000

    Troubleshooting advance()

--- horseRace.cpp
+++ horseRace.cpp
@@ -1,27 +1,39 @@
 # include <iostream>
 # include <random>
 
+const LANE_LENGTH = 15
 void advance(int horseNum, int* horses);
 void printLane(int horseNum, int* horses);
-bool isWinner(int horseNum, int* horses);
+//bool isWinner(int horseNum, int* horses);
 
 int main(){
-	const LANE_LENGTH = 14;
-	horses = [0, 0, 0, 0, 0];
-	for (i in horses){
-		horseNum = horses[i];
+	int horses = [0, 0, 0, 0, 0];
+	for (int i = 0; horses[i]; i++){
+		horseNum& = horses[i];
 		advance(horseNum, int* horses);
-		printLane(horseNum, int*horses);
+		printLane(horseNum, int* horses);
 	}//End for loop
+	return 0;
 }//End main
 
-int advance(int number, int* array){
+void advance(int number, int* array){
 	std::random_device rd;
 	std::uniform_int_distribution<int> dist(0, 1);
-
-	coin = dist(rd);
+	int coin = dist(rd);
+	std::cout << coin << std::endl;
 	if(coin == 1){
-		array[i]++;
+		array[number]++;
 	} //End if
 }//End advance
 
+void printLane(int number, int* array){
+	for (int i = 0; i < LANE_LENGTH; i++){
+		if (i == array[number]){
+			std::cout << number;
+		}
+		else {
+			std::cout << '.';
+		}
+	}// end for loop
+	std::cout << std::endl;
+}// End printLane
